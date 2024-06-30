5f047718bb4b2027aa7d03c780e88d54dc482464
DESKTOP-KBLEMG7\1 <artem.chesnokov@beterra.ru>
2024-07-01 00:19:42 +0300

    6sprint pull

--- src/service/History/InMemoryHistoryManager.java
+++ src/service/History/InMemoryHistoryManager.java
@@ -8,21 +8,24 @@ public class InMemoryHistoryManager implements HistoryManager {
     private Map<Integer, Task> historyList = new LinkedHashMap<>();
 
     @Override
-    public void add(Task task){
-        if(historyList.containsKey(task.getId())){
+    public void add(Task task) {
+        if (historyList.containsKey(task.getId())) {
             historyList.remove(task.getId());
         }
         historyList.put(task.getId(), task);
     }
+
     @Override
     public void remove(int id) {
         historyList.remove(id);
     }
+
     @Override
-    public List<Task> getHistory(){
+    public List<Task> getHistory() {
         return getTask(historyList);
     }
-    public List<Task> getTask(Map historyList){
+
+    public List<Task> getTask(Map historyList) {
         return new ArrayList<>(historyList.values());
     }
 }
