1a8ffc9303e911282778a11914758ef6f98164b8
DESKTOP-KBLEMG7\1 <artem.chesnokov@beterra.ru>
2024-07-01 00:54:31 +0300

    6sprint buld task Task commit test style 6

--- src/service/Task/InMemoryTaskManager.java
+++ src/service/Task/InMemoryTaskManager.java
@@ -59,25 +59,25 @@ public class InMemoryTaskManager implements TaskManager {
 
     @Override
     public Subtask getSubtasksToId(Integer id) {
-        if (id != null){
-            historyManager.add( subTasks.get(id) );
+        if ( id != null ) {
+            historyManager.add( subTasks.get( id ) );
         }
         return subTasks.get(id);
     }
 
     @Override
-    public List<Task> allTask(){
-        return new ArrayList<>( tasks.values() );
+    public List<Task> allTask() {
+        return new ArrayList<>(tasks.values());
     }
 
     @Override
-    public List<Epic> allEpic(){
-        return new ArrayList<>( epics.values() );
+    public List<Epic> allEpic() {
+        return new ArrayList<>(epics.values());
     }
 
     @Override
-    public List<Subtask> allSubtask(){
-        return new ArrayList<>( subTasks.values() );
+    public List<Subtask> allSubtask() {
+        return new ArrayList<>(subTasks.values());
     }
 
     @Override
@@ -137,9 +137,9 @@ public class InMemoryTaskManager implements TaskManager {
 
     @Override
     public void updateStatusEpic(ArrayList<Subtask> listSubtaskInEpic, Epic epics) {
-        if ( listSubtaskInEpic.stream().allMatch(item -> item.status.equals( TaskStatus.NEW ))) {
+        if (listSubtaskInEpic.stream().allMatch(item -> item.status.equals(TaskStatus.NEW))) {
             epics.status = TaskStatus.NEW;
-        } else if ( listSubtaskInEpic.stream().allMatch(item -> item.status.equals( TaskStatus.DONE ))) {
+        } else if (listSubtaskInEpic.stream().allMatch(item -> item.status.equals(TaskStatus.DONE))) {
             epics.status = TaskStatus.DONE;
         } else {
             epics.status = TaskStatus.IN_PROGRESS;
@@ -147,7 +147,7 @@ public class InMemoryTaskManager implements TaskManager {
     }
 
     @Override
-    public List<Subtask> allSubtaskInEpic(Integer id){
+    public List<Subtask> allSubtaskInEpic(Integer id) {
         return epics.get(id).getListSubtask();
     }
 
