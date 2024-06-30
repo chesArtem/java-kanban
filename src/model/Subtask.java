f6148569ac10a7a040b3a44a0a8273afcf014ca3
DESKTOP-KBLEMG7\1 <artem.chesnokov@beterra.ru>
2024-07-01 01:03:28 +0300

    6sprint buld task Epic Subtask commit test style 1

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -2,6 +2,7 @@ package model;
 
 public class Subtask extends Task{
     private Epic parentEpic;
+
     public Subtask(int id, String title, String info, Epic parentEpic) {
         super(id, title, info);
         this.parentEpic = parentEpic;
