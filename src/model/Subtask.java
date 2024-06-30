1e2a2fe8001e103506793847b6e00af8655c7d47
DESKTOP-KBLEMG7\1 <artem.chesnokov@beterra.ru>
2024-07-01 01:05:28 +0300

    6sprint buld task Subtask commit test style 2

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -1,6 +1,6 @@
 package model;
 
-public class Subtask extends Task{
+public class Subtask extends Task {
     private Epic parentEpic;
 
     public Subtask(int id, String title, String info, Epic parentEpic) {
